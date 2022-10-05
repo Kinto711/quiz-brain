964747e5cb4ed51e3d76914b4c3c2d6157593a41
Alexander Pashkou <64390864+Kinto711@users.noreply.github.com>
2022-10-05 16:44:49 -0400

    QuizGame

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-j
+from question_model import Question
+from data import question_data
+
+
+question_bank = []
+
+for q in question_data:
+    # q_text = q["text"]
+    # q_answer = q["answer"]
+    # new_question = Question(q_text, q_answer)
+    # question_bank.append(new_question)
+    new_question = Question(q["text"], q["answer"])
+    question_bank.append(new_question)
+
+
+print(question_bank)
